e2cbcf21dbd8839d0b48611f62bfb45cb311f094
guardiantzb <3301344200@qq.com>
2025-04-29 20:32:59 +0800

    cnm

--- src/main/java/tzb/pojo/VideoAlbum.java
+++ src/main/java/tzb/pojo/VideoAlbum.java
@@ -1,6 +1,8 @@
-package com.sample;
+package tzb.pojo;
 
+import lombok.Data;
 
+@Data
 public class VideoAlbum {
 
   private long id;
@@ -8,50 +10,4 @@ public class VideoAlbum {
   private String description;
   private String coverUrl;
   private java.sql.Timestamp createTime;
-
-
-  public long getId() {
-    return id;
-  }
-
-  public void setId(long id) {
-    this.id = id;
-  }
-
-
-  public String getTitle() {
-    return title;
-  }
-
-  public void setTitle(String title) {
-    this.title = title;
-  }
-
-
-  public String getDescription() {
-    return description;
-  }
-
-  public void setDescription(String description) {
-    this.description = description;
-  }
-
-
-  public String getCoverUrl() {
-    return coverUrl;
-  }
-
-  public void setCoverUrl(String coverUrl) {
-    this.coverUrl = coverUrl;
-  }
-
-
-  public java.sql.Timestamp getCreateTime() {
-    return createTime;
-  }
-
-  public void setCreateTime(java.sql.Timestamp createTime) {
-    this.createTime = createTime;
-  }
-
 }
